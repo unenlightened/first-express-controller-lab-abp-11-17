fb3cfa7f4dd5c27048708de5ca4b1a840f5bd021
aviflombaum <avi@flombaum.com>
2017-11-19 14:14:00 -0500

    Avi examples

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
-
+const SiteController = require("./controllers/SiteController.js")
+const PhotosController = require("./controllers/PhotosController.js")
 
 // Routes
-
+app.get("/", SiteController.Index)
+app.get("/about", SiteController.About)
+app.get("/contact", SiteController.Contact)
+app.get("/photos", PhotosController.Index)
+app.get("/photos/new", PhotosController.New)
 
 module.exports = app
 
--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,16 @@
 const SiteController = {};
 
+SiteController.Index = function(req, resp){
+
+  resp.send("Hi.")
+};
+
+SiteController.About = function(req, resp){
+
+};
+
+SiteController.Contact = function(req, resp){
+
+};
 
 module.exports = SiteController;
